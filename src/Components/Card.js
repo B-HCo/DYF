763c06ecf1e123b827299ec6bc211e36368d05ec
bethq321 <bethqualls@comcast.net>
2024-06-07 09:34:32 -0500

    finished draft of first card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,12 +4,18 @@ const Card = () => {
     return (
         <div className="container">
             <div className="card">
+                <img src="/../../public/assets/oranges-carrots.png" />
                 <div className="imgBx">
                     <img src="/../../public/assets/orange-drink-bottle.png" />
                 </div>
                 <div className = "contentBx">
                     <h2>Sublime Orange Carrot</h2>
-                    <p>12 oz</p>
+                    <div class="size">
+                        <h3>Size : </h3>
+                        <span>8 oz</span>
+                        <span>12 oz</span>
+                        <span>16 oz</span>
+                    </div>
                     <p>$4.99</p>
                     <a href = "#">Buy Now</a>
                 </div>
